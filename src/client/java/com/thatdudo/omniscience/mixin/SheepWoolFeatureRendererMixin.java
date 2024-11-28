49f87cfdf8edfb177876605f64eb83e913b89424
thatDudo <thatdudo@proton.me>
2024-11-28 13:57:06 +0100

    Fix invisible sheep wool color

--- src/client/java/com/thatdudo/omniscience/mixin/SheepWoolFeatureRendererMixin.java
+++ src/client/java/com/thatdudo/omniscience/mixin/SheepWoolFeatureRendererMixin.java
@@ -40,12 +40,13 @@ public class SheepWoolFeatureRendererMixin {
         Config config = ConfigManager.getConfig();
 
         if (config.isEnabled() && sheepEntity.isInvisible() && !sheepEntity.isSheared() && config.isEntityTargeted(sheepEntity)) {
-
+            // Sheep wool needs to be rendered manually since its not rendered if the sheep is invisible
+            float[] woolColors = SheepEntity.getRgbColor(sheepEntity.getColor());
             _context.getModel().copyStateTo(this.model);
             this.model.animateModel(sheepEntity, f, g, h);
             this.model.setAngles(sheepEntity, f, g, j, k, l);
             VertexConsumer vertexConsumer2 = vertexConsumerProvider.getBuffer(RenderLayer.getItemEntityTranslucentCull(SKIN));
-            this.model.render(matrixStack, vertexConsumer2, i, LivingEntityRenderer.getOverlay(sheepEntity, 0.0f), 1.0f, 1.0f, 1.0f, 0.15f);
+            this.model.render(matrixStack, vertexConsumer2, i, LivingEntityRenderer.getOverlay(sheepEntity, 0.0f), woolColors[0], woolColors[1], woolColors[2], 0.15f);
         }
     }
 }
